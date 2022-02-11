bc241e8996ef8df631a3f30d4031604b42561608
oumash <oumayma.chakkour@gmail.com>
2022-02-11 11:21:39 +0100

    ok

--- src/master/Master.java
+++ src/master/Master.java
@@ -5,6 +5,7 @@ public class Master {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("first test ");
+		System.out.print("second test 1 ");
 
 	}
 
