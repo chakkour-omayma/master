02534a1de537734f195146039599207435094423
Nezha-A <guergarinezha@gmail.com>
2022-02-12 17:39:20 +0100

    second try

--- src/master/Master.java
+++ src/master/Master.java
@@ -5,6 +5,11 @@ public class Master {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("first test ok ");
+		System.out.print("second test ok ");
+		System.out.print("second test ok ");
+		System.out.print("second test ok ");
+		System.out.print("second test ok ");
+		
 
 	}
 
